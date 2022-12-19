f49b45f73b30b9b26e04cfdf235679f33090a699
vitalii-borisiuk <vitalii.borisiuk@gmail.com>
2022-12-19 15:00:23 +0200

    edit hw-06

--- js/task-06.js
+++ js/task-06.js
@@ -3,7 +3,6 @@ const input = document.querySelector('#validation-input');
 input.addEventListener('blur', inputLength);
 
 function inputLength(event) {
-    console.log(input);
     if (event.target.value.length >= 6) {
         input.classList.remove('invalid'); 
         input.classList.add('valid'); 
